d04c3b9ce82e0472ef87039b83e53308455d53d6
Maddox Twin1 <jaspertena@aol.com>
2022-05-06 19:02:30 -0400

    create function for classes

--- server.js
+++ server.js
@@ -1,7 +1,53 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
-const jest = require('jest')
 
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+const Manager = require("./lib/Manager")
 
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee email?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What role is the employee?",
+        choices: ["Manager", "Engineer", "Intern"]
+    }
+]
 
-jest()
+function promptUser(){
+    inquirer.prompt(questions)
+    .then(function(response){
+        console.log(response)
+        if(response.role==="Manager"){
+            addManager(response)
+        }
+        else if(response.role==="Engineer"){
+            addEngineer(response)
+        }
+        else if(response.role==="Intern"){
+            addIntern(response)
+        }
+
+    })
+}
+
+promptUser()
+
+function addManager(response) {}
+function addEmployee(response) {}
+function addIntern(response) {}
